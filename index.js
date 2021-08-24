065ecc02bd4770612e4feac48c4190cfcd9393a5
suraj more <29055479+surajmore98@users.noreply.github.com>
2021-08-25 00:41:20 +0530

    minionTranslator completed.

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 let input = prompt("Please enter your Name!!");
 alert("Welcome to the Banana Talk " + input + " !\nLets start the game.");
+
+var button  = document.querySelector("#btn");
+var inputArea  = document.querySelector("#input-text");
+var outputArea = document.querySelector("#output-text");
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+function getRequestUrl(input) {
+    return url + "?text=" + input;
+};
+
+function errorHandler(error) {
+    console.log(error);
+    alert("We have error with API");
+};
+
+button.addEventListener("click" , () => {
+    var inputText = inputArea.value;
+    fetch(getRequestUrl(inputText))
+        .then(response => response.json())
+        .then(data => {
+            var output  = data.contents.translated;
+            outputArea.innerText = output;
+        }).catch(error => errorHandler(error));
+});
